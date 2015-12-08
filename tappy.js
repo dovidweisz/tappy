9380fc612a253f29366ace8ad715e0841dc7e13d
davidwe <davidwe@bhphoto.com>
2015-12-08 18:37:46 -0500

    rewrite that tries not to interfere when not looking for tap event

--- tappy.js
+++ tappy.js
@@ -1,41 +1,84 @@
 (function() {
-	if (window.MouseEvent && ! window.tappy){
-		var tapInProgress;
-		document.addEventListener("click", function(e){
-			if(e.isTrusted){
-				simulateEvent(e.srcElement, "nontapclick");
-			}
-		});
-		document.addEventListener("touchstart", function(e){
-			e.preventDefault();
-			var clickDefaultAllowed = simulateEvent(e.srcElement, "click");
+	
+	// tapInProgress handle can be shared between instances as they 
+	var tapInProgress,
+		TAPTIME = 300;
+
+	function Tappy (node) {
+		if (window.MouseEvent){
+			this.init(node);
+			this.node = node;
+		}
+	}
+
+	Tappy.prototype.init = function(node) {
+		node.addEventListener("touchstart", touchStartEvent, true);
+		node.addEventListener("touchend", touchEndEvent, true);
+		node.addEventListener("click", clickEvent, true);
+	};
+
+	Tappy.prototype.destroy = function() {
+		this.node.removeEventListener("touchstart", touchStartEvent, true);
+		this.node.removeEventListener("touchend", touchEndEvent, true);
+		this.node.removeEventListener("click", clickEvent, true);
+	};
+
+	function touchStartEvent(e){
+		// check for tapInProgress make sure it's not a double
+		if(! tapInProgress || tapInProgress.el !== e.srcElement){
 			tapInProgress = {
 				"el" : e.srcElement,
-				"time" : new Date,
-				"clickDefaultAllowed" : clickDefaultAllowed
+				"startTime" : new Date
 			}
-		}, true);
-
-		document.addEventListener("touchend", function(){
-			if(tapInProgress && (new Date) - tapInProgress.time <= 200 ){
-				simulateEvent(tapInProgress.el, "tap", {
-					"clickDefaultAllowed" : tapInProgress.clickDefaultAllowed
-				});
-				tapInProgress = null;
-			} 
-		}, true);
-
-		function simulateEvent(el, evType, ext) {
-			var event = new MouseEvent(evType, {
-				"view": window,
-				"bubbles": true,
-				"cancelable": true
-			}); 
-			if(ext){
-				_.extend(event, ext);
-			}
-			return el.dispatchEvent(event);
 		}
-		window.tappy = true;
 	}
+	function touchEndEvent(e){
+		//                   Check for double again
+		if( tapActive() && ! tapInProgress.tapEvent){
+			var tapEvent = simulateEvent(tapInProgress.el, "tap");
+			tapInProgress.tapEvent = tapEvent;
+		} 
+	}
+	function clickEvent (e) {
+		if( tapActive() && tapInProgress.tapEvent ){
+			var tapEvent = tapInProgress.tapEvent;
+			e.tapEvent = tapEvent;
+			if(! tapEvent.dispatchReturnValue){
+				if(tapEvent.defaultPrevented){
+					e.preventDefault();
+				}else{
+					e.stopPropagation();
+				}
+			}
+			tapInProgress = null;
+		}else{
+			simulateEvent(e.srcElement, "nontapclick");
+		}
+	}
+	
+	function tapActive(){
+		if(tapInProgress){
+			if((new Date) - tapInProgress.startTime <= TAPTIME){
+				return true;
+			}
+			tapInProgress = null;
+		}
+		return false;
+	}
+
+	function simulateEvent(el, evType) {
+		var event = new MouseEvent(evType, {
+			"view": window,
+			"bubbles": true,
+			"cancelable": true
+		}); 
+		event.dispatchReturnValue = el.dispatchEvent(event);
+		return event;
+	}
+
+	if(! window.Tappy){
+		window.Tappy = Tappy;
+		Tappy.documentInstance = new Tappy(document);
+	}
+
 })();
